8ab71caf359d1f80899855900e2543a749b07876
ajmalsarwary <sarwary.ey@outlook.com>
2024-01-31 16:45:50 +0100

    added gbm page to portfolio

--- app/components/HomeComponent.js
+++ app/components/HomeComponent.js
@@ -1,10 +1,13 @@
 // app/components/HomeComponent.js
 
+import React from 'react';
+import GbmSimulation from './GbmSimulation/GbmSimulation'; // Make sure the path is correct
 import styles from './HomeComponent.module.css';
 
 function HomeComponent() {
   return (
     <div className={styles.container}>
+      {/* Your existing header and navigation */}
       <header className={styles.headerFooter}>
         <nav>
           <ul className={styles.navList}>
@@ -20,10 +23,15 @@ function HomeComponent() {
           </ul>
         </nav>
       </header>
+
+      {/* Main content area */}
       <main className={styles.mainContent}>
         <h1 className={styles.titleH1}>A curated display of my work</h1>
         <p>Welcome to a showcase of my recent work and a sneak peek into my ongoing projects.</p>
+        <GbmSimulation /> {/* GBM Simulation Component */}
       </main>
+
+      {/* Your existing footer */}
       <footer className={styles.headerFooter}>
         <p>© 2023 A.S. </p>
       </footer>
@@ -32,4 +40,5 @@ function HomeComponent() {
 }
 
 export default HomeComponent;
+
 
